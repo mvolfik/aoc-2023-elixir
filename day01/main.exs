02b5672c60d050820209a0c04776f6065fe7158b
Matěj Volf <git@mvolfik.com>
2023-12-01 12:24:47 +0100

    01.2

--- day01/main.exs
+++ day01/main.exs
@@ -5,19 +5,51 @@ defmodule Main do
         0
 
       line ->
-        (line
-         |> String.graphemes()
-         |> Enum.flat_map(fn c ->
-           case Integer.parse(c) do
-             :error -> []
-             {n, _} -> [n]
-           end
+        (Regex.scan(
+           ~r/[0-9]|on(?=e)|tw(?=o)|thre(?=e)|four|fiv(?=e)|six|seve(?=n)|eigh(?=t)|nin(?=e)/,
+           line
+         )
+         |> Enum.flat_map(fn
+           ["on"] ->
+             [1]
+
+           ["tw"] ->
+             [2]
+
+           ["thre"] ->
+             [3]
+
+           ["four"] ->
+             [4]
+
+           ["fiv"] ->
+             [5]
+
+           ["six"] ->
+             [6]
+
+           ["seve"] ->
+             [7]
+
+           ["eigh"] ->
+             [8]
+
+           ["nin"] ->
+             [9]
+
+           [c] ->
+             case Integer.parse(c) do
+               #  :error -> []
+               {n, _} -> [n]
+             end
          end)
          |> List.foldr(:empty, fn
            x, :empty -> {x, x}
            x, {_, b} -> {x, b}
          end)
          |> then(fn {x, y} -> 10 * x + y end)) + calc()
+
+        #  |> then(fn x -> [x] end)) ++ calc()
     end
   end
 end
