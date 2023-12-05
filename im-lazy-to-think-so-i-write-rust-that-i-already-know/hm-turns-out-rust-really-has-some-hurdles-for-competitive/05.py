cf150f46cf56a56aaeb9c6b0781b4c3feb6ada52
Matěj Volf <git@mvolfik.com>
2023-12-05 12:46:59 +0100

    05.2

--- im-lazy-to-think-so-i-write-rust-that-i-already-know/hm-turns-out-rust-really-has-some-hurdles-for-competitive/05.py
+++ im-lazy-to-think-so-i-write-rust-that-i-already-know/hm-turns-out-rust-really-has-some-hurdles-for-competitive/05.py
@@ -1,4 +1,5 @@
 see = [int(x) for x in input().split(": ")[1].split()]
+see = [(see[x], see[x + 1]) for x in range(0, len(see), 2)]
 
 
 def ll():
@@ -15,28 +16,52 @@ while (l := ll()) is not None:
         continue
 
     if l.endswith("map:"):
-        current = {}
+        if current is not None:
+            current.sort()
+        current = []
         s = l.split()[0].split("-")
         maps[s[0]] = (s[2], current)
         continue
 
     a, b, c = map(int, l.split())
-    current[b] = (a, c)
+    current.append((b, a, c))
 
-low = 9999999999999
-for s in see:
-    k = "seed"
-    while k != "location":
-        print(k, s)
-        k, m = maps[k]
-        for start,(target,len) in m.items():
-            offset = s - start
-            if offset >= 0 and offset < len:
-                s = target + offset
+
+def process(k, start1, len1):
+    print(k, start1, len1)
+    if k == "location":
+        return start1
+    (next, m) = maps[k]
+    low = 999999999999
+    for start, target, len in m:
+        offset = start1 - start
+        if offset < 0:
+            seg_len = min(len1, -offset)
+            print("copy seg", start1, seg_len)
+            low = min(low, process(next, start1, seg_len))
+            start1 += seg_len
+            len1 -= seg_len
+            if len1 == 0:
                 break
-        else:
-            pass
-            # raise Exception("no path")
 
-    low = min(low, s)
+        offset = start1 - start
+        assert offset >= 0
+        if offset < len:
+            seg_len = min(len1, len - offset)
+            low = min(low, process(next, target + offset, seg_len))
+            start1 += seg_len
+            len1 -= seg_len
+            if len1 == 0:
+                break
+
+    if len1 > 0:
+        low = min(low, process(next, start1, len1))
+
+    return low
+
+
+low = 999999999999
+for s, l in see:
+    low = min(low, process("seed", s, l))
+
 print(low)
