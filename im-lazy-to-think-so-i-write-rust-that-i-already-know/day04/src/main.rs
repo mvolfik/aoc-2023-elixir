e3e8ff7edacd073afb9c3e3ca64f247d6f8a3525
Matěj Volf <git@mvolfik.com>
2023-12-05 12:13:42 +0100

    04.2

--- im-lazy-to-think-so-i-write-rust-that-i-already-know/day04/src/main.rs
+++ im-lazy-to-think-so-i-write-rust-that-i-already-know/day04/src/main.rs
@@ -1,13 +1,16 @@
 #![feature(let_chains)]
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::io::stdin;
 
 fn main() {
     let mut buf = String::new();
     let mut total = 0;
+    let mut copies = HashMap::new();
+    let mut i = 0;
     while let Ok(len) = stdin().read_line(&mut buf)
         && len > 1
     {
+        i += 1;
         assert_eq!(buf.pop(), Some('\n'));
         let mut split = buf.split(": ").nth(1).unwrap().split(" | ");
         let c1 = split
@@ -19,22 +22,18 @@ fn main() {
             .collect::<HashSet<u64>>();
 
         let mut sum = 0;
-        for x in split
-            .next()
-            .unwrap()
-            .split(' ')
-            .filter(|x| !x.is_empty())
-        {
+        for x in split.next().unwrap().split(' ').filter(|x| !x.is_empty()) {
             let x = x.parse().unwrap();
             if c1.contains(&x) {
-                if sum == 0 {
-                    sum = 1;
-                } else {
-                    sum = sum * 2;
-                }
+                sum += 1;
             }
         }
-        total += sum;
+        let my_copies = *copies.get(&i).unwrap_or(&1);
+        println!("{}: {} {}", i, sum, my_copies);
+        for j in i..=i + sum {
+            *copies.entry(j).or_insert(1) += my_copies;
+        }
+        total += my_copies;
         buf.clear();
     }
     println!("{}", total)
