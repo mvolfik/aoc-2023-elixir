c4ae3e012dad14b51e51b0726c59b39c126455de
Matěj Volf <git@mvolfik.com>
2023-12-06 09:25:38 +0100

    06.2

--- im-lazy-to-think-so-i-write-rust-that-i-already-know/hm-turns-out-rust-really-has-some-hurdles-for-competitive/06.py
+++ im-lazy-to-think-so-i-write-rust-that-i-already-know/hm-turns-out-rust-really-has-some-hurdles-for-competitive/06.py
@@ -1,14 +1,9 @@
-l1 = [int(x) for x in input().split()[1:]]
-l2 = [int(x) for x in input().split()[1:]]
+a = int("".join(input().split()[1:]))
+b = int("".join(input().split()[1:]))
 
-x = 1
-for i in range(len(l1)):
-    a = l1[i]
-    b = l2[i]
-    n = 0
-    for j in range(1, a):
-        d = j*(a-j)
-        if d > b:
-            n += 1
-    x *= n
-print(x)
+n = 0
+for j in range(1, a):
+    d = j*(a-j)
+    if d > b:
+        n += 1
+print(n)
