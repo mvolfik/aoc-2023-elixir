10dbbe718bde66677887554e04bd7c644783cb2a
Matěj Volf <git@mvolfik.com>
2023-12-15 13:38:21 +0100

    15.2

--- im-lazy-to-think-so-i-write-rust-that-i-already-know/hm-turns-out-rust-really-has-some-hurdles-for-competitive/15.py
+++ im-lazy-to-think-so-i-write-rust-that-i-already-know/hm-turns-out-rust-really-has-some-hurdles-for-competitive/15.py
@@ -8,7 +8,7 @@ def ll():
         return None
 
 
-s = 0
+boxes = [[] for _ in range(256)]
 while (l := ll()) is not None:
     if not l:
         continue
@@ -17,9 +17,34 @@ while (l := ll()) is not None:
         if not x:
             continue
 
+        n = None
+        if x[-1] == "-":
+            label = x[:-1]
+        else:
+            label, n = x.split("=")
+
         v = 0
-        for c in x:
+        for c in label:
             v = (v + ord(c)) * 17 % 256
-        s += v
-        print(v)
+
+        box = boxes[v]
+        if n is None:
+            for i in range(len(box)):
+                if box[i][0] == label:
+                    boxes[v] = box[:i] + box[i + 1 :]
+                    break
+        else:
+            for i in range(len(box)):
+                if box[i][0] == label:
+                    box[i] = (label, int(n))
+                    break
+            else:
+                box.append((label, int(n)))
+        print(x)
+        print(boxes)
+
+s = 0
+for i in range(len(boxes)):
+    for j in range(len(boxes[i])):
+        s += boxes[i][j][1] * (i + 1) * (j + 1)
 print(s)
